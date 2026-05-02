8ab0bcb677053129f54ea69847d9e90fec0d10ae
Aamir Malick <m.aamirmalick@gmail.com>
2026-05-02 20:10:48 +0530

    initial-commit

---
+++ src/main/java/com/aamirtechie/DemoAppApplication.java
@@ -0,0 +1,14 @@
+package com.aamirtechie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+public class DemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAppApplication.class, args);
+    }
+
+}
